dd35062d7ab634dee8bed020461529fcfe75ce15
qmonmert <quentin.monmert@gmail.com>
2020-11-22 18:09:09 +0100

    v2

--- src/sum-of-pairs.ts
+++ src/sum-of-pairs.ts
@@ -2,15 +2,16 @@ export const sumOfPairs = (ints: number[], s: number): [number, number] | void =
   if (ints.length === 0) {
     return undefined;
   }
-  let result: [number, number] = undefined;
-  let indice = Number.MAX_VALUE;
-  for (let i = 0; i < ints.length; i++) {
-    for (let j = 0; j < ints.length; j++) {
-      if (i !== j && ((ints[i] + ints[j]) === s) && indice > Math.max(i, j)) {
-        indice = Math.max(i, j);
-        result = [ints[i], ints[j]];
+  const res = ints.map((v1, i) => {
+    return ints.map((v2, j) => {
+      if (i !== j && ((v1 + v2) === s)) {
+        return [ints[i], ints[j], Math.max(i, j)];
       }
-    }
-  }
-  return result;
+    }).sort(compareIndice)[0];
+  }).sort(compareIndice)[0];
+  return res ? [res[0], res[1]] : undefined;
+}
+
+const compareIndice = (i1: [number, number, number], i2: [number, number, number]) => {
+  return (i1 && i2) ? i1[2] - i2[2] : 0;
 }
